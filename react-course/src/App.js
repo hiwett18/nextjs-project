4315fdb75b91aa9c67adfb60b4dd7587cbc26c7a
Hiwot Woldesemayat <hiwot.woldesemayat@fdmgroup.com>
2023-07-11 19:05:36 -0400

    modal and backdrop

---
+++ react-course/src/App.js
@@ -0,0 +1,17 @@
+import Backdrop from "./components/Backdrop";
+import Modal from "./components/Modal";
+import Todo from "./components/Todo";
+function App() {
+  return (
+    <div>
+      <h1>My Todos</h1>
+        <Todo text='Learn React'/>
+        <Todo text='Master React'/>
+        <Todo text='Explore the full course of React'/>
+        
+        
+    </div>
+  );
+}
+
+export default App;
