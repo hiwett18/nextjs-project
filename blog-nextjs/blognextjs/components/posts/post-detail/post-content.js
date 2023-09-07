81c00c946c954a95a9f515bfeb6e7e68601ad14c
Hiwot Woldesemayat <hiwot.woldesemayat@fdmgroup.com>
2023-09-06 23:58:06 -0400

    prepared for deployment

--- blog-nextjs/blognextjs/components/posts/post-detail/post-content.js
+++ blog-nextjs/blognextjs/components/posts/post-detail/post-content.js
@@ -1,10 +1,16 @@
 import ReactMarkdown from 'react-markdown';
 import Image from 'next/image';
-import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
-import { atomDark } from 'react-syntax-highlighter/dist/cjs/styles/prism';
+import { PrismLight as SyntaxHighlighter } from 'react-syntax-highlighter';
+import atomDark from 'react-syntax-highlighter/dist/cjs/styles/prism/atom-dark';
+import js from 'react-syntax-highlighter/dist/cjs/languages/prism/javascript'
+import css from 'react-syntax-highlighter/dist/cjs/languages/prism/css'
 
 import PostHeader from './post-header';
 import classes from './post-content.module.css';
+
+SyntaxHighlighter.registerLanguage('js', js);
+SyntaxHighlighter.registerLanguage('css', css);
+
 
 function PostContent(props) {
   const { post } = props;
@@ -12,16 +18,7 @@ function PostContent(props) {
   const imagePath = `/images/posts/${post.slug}/${post.image}`;
 
   const customRenderers = {
-    // img(image) {
-    //   return (
-    //     <Image
-    //       src={`/images/posts/${post.slug}/${image.src}`}
-    //       alt={image.alt}
-    //       width={600}
-    //       height={300}
-    //     />
-    //   );
-    // },
+   
     p(paragraph) {
       const { node } = paragraph;
 
@@ -45,7 +42,7 @@ function PostContent(props) {
 
     code(code) {
       const { className, children } = code;
-      const language = className.split('-')[1]; // className is something like language-js => We need the "js" part here
+      const language = className.split('-')[1]; 
       return (
         <SyntaxHighlighter
           style={atomDark}
